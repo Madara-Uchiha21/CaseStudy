94f39229c856dfa06171d85787dbc87e147bdc72
Priyanshu Kumar <146458694+Madara-Uchiha21@users.noreply.github.com>
2025-04-07 23:13:02 +0530

    Add files via upload

---
+++ Case1USdata.py
@@ -0,0 +1,15 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %%
+df = pd.read_csv("C:\\Users\\rajpr\\OneDrive\\Desktop\\CODING\\DataScience\\Case Studies\\US_honey_dataset.csv")
+df.head()
+
+# %%
+df.info()
+df.describe()
+
+# %%
